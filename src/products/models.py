f4957914779d405ee7fed863f9d874cb269466cb
Ahmed Al-ridha <57795398+freshAhmed@users.noreply.github.com>
2025-04-21 18:38:34 -0400

    Delete src directory

--- src/products/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-
-class Product(models.Model):
-  title =models.CharField(max_length=10)
-  price =models.DecimalField(decimal_places=3,max_digits=10)
-  description=models.TextField(blank=True)
-  summary=models.TextField(default='cool!') 
